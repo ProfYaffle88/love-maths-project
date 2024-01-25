7382978a4fabca968f35e74cdc19006c40f84d66
James Price <james.a.t.price@gmail.com>
2024-01-25 12:25:48 +0000

    Added function structure to script file.

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+//Main Game function
+function runGame() {
+
+}
+
+//Check answers and change scores
+function checkAnswer() {
+
+}
+
+function calculateCorrectScore() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+//Display questions
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
